b30d4663cdb8255631b26c4e71493c5332f04a23
jaennil <jaennil@yandex.ru>
2024-01-16 23:49:21 +0300

    add map that syncs with panorama

--- map.ts
+++ map.ts
@@ -7,12 +7,19 @@ interface Point {
 }
 
 function init() {
-  ymaps.panorama
-    .createPlayer(
-      "panorama",
-      [55.65336771654587, 37.52289044973747]
-    )
-    .done((player: any) => {
+  const myMap = new ymaps.Map("map", {
+    center: [55.65336771654587, 37.52289044973747],
+    zoom: 18,
+    type: "yandex#map",
+    controls: ["typeSelector"],
+  });
+
+  myMap.getPanoramaManager().then(function (manager: any) {
+    manager.enableLookup();
+    manager.openPlayer(myMap.getCenter());
+
+    manager.events.add("openplayer", function () {
+      const player = manager.getPlayer();
       const canvas = document.querySelector(".ymaps-2-1-79-panorama-screen");
       if (canvas === null) {
         console.error("cant find canvas");
@@ -24,35 +31,36 @@ function init() {
 
       const points: Point[] = createPoints(100, width, height);
 
+      points.forEach((point) => {
+        canvas.appendChild(point.element);
+        animatePoint(point);
+      });
+
       let bearing = player.getDirection()[0];
       let pitch = player.getDirection()[1];
+
       player.events.add("directionchange", function () {
-        let new_bearing = player.getDirection()[0];
-        let new_pitch = player.getDirection()[1];
+        const new_bearing = player.getDirection()[0];
+        const new_pitch = player.getDirection()[1];
+
         let delta_bearing = bearing - new_bearing;
         let delta_pitch = pitch - new_pitch;
+
         let horizontal_span = player.getSpan()[0];
         let vertical_span = player.getSpan()[1];
-        console.log(horizontal_span);
-        console.log((delta_bearing / horizontal_span) * width);
+
         points.forEach((point) => {
           point.x += (delta_bearing / horizontal_span) * width;
           point.y += (delta_pitch / vertical_span) * height;
           point.element.style.left = point.x + "px";
           point.element.style.top = point.y + "px";
         });
+
         bearing = new_bearing;
         pitch = new_pitch;
-        console.log(player.getDirection());
-        console.log(player.getSpan());
-      });
-
-      // Add points to the panorama
-      points.forEach((point) => {
-        canvas.appendChild(point.element);
-        animatePoint(point);
       });
     });
+  });
 
   function createPoints(count: number, width: number, height: number) {
     const result: Point[] = [];
@@ -84,7 +92,7 @@ function init() {
       point.y += Math.random() * 2 - 1;
       point.element.style.left = point.x + "px";
       point.element.style.top = point.y + "px";
-    }, 5);
+    }, 1);
   }
 }
 
